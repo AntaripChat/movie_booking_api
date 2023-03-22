d80ed1b87d91491e563c6c566a911def9ca96884
arpurwar <arpurwar@paypal.com>
2023-03-22 21:51:40 +0530

    model creation

--- models/movies.models.js
+++ models/movies.models.js
@@ -8,7 +8,49 @@ const movieSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    casts: {
+        type: [String],
+        required: true
+        
+    },
+    trailerUrl :{
+        type: String,
+        required: true
+        
+    },
+    posterUrl :{
+        type: String,
+        required: true
+        
+    },
+    language :{
+        type : String,
+        required :true,
+        default: "Hindi"
+
+    },
+    releaseDate :{
+        type: String,
+        required :true
+        
+    },
+    director : {
+        type: String,
+        required :true
+    },
+    releaseStatus : {
+        type : String,
+        required : true,
+        default : "RELEASED"
+        //Other possible values could be UNRELEASED | BLOCKED
+    },
+    updatedAt: {
+        type: Date,
+        default: () => {
+            return Date.now();
+        }
+    }  
 })
 
 const movieModel = mongoose.model('Movie', movieSchema);
