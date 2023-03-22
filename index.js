536d91c17d1a4ec48627ff817d3f1e1f00ca8590
arpurwar <arpurwar@paypal.com>
2023-03-22 22:34:06 +0530

    default entries

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-require('./models/movies.models');
+const Movie = require('./models/movies.models');
 const { PORT } = require('./configs/server.config');
 const { DB_URL } = require('./configs/db.config');
 
@@ -11,6 +11,7 @@ const { DB_URL } = require('./configs/db.config');
     try{    
         await mongoose.connect(DB_URL);
         console.log('db connected');
+        await init();
     }
     catch(err){
         console.error('error getting while connecting mongoDB', err);
@@ -18,6 +19,69 @@ const { DB_URL } = require('./configs/db.config');
 
 })();
 
+async function init(){
+    try{
+    await Movie.create({
+        name: "Bachhan Pandey",
+        description: "Comedy Masala Movie",
+        casts: ["Akshay Kumar", "Jacqueline Fernandiz"],
+        director: "Farhad Samji",
+        trailerUrl: "http://bacchanpandey/trailers/1",
+        posterUrl: "http://bacchanpandey/posters/1",
+        language: "Hindi",
+        releaseDate: "18-03-2022",
+        releaseSatus: "RELEASED"
+    });
+    await Movie.create({
+        name: "Jalsa",
+        description: "Intense Drama Movie",
+        casts: ["Vidya Balan", "Shefali Shah"],
+        director: "Suresh Triveni",
+        trailerUrl: "http://jalsa/trailers/1",
+        posterUrl: "http://jalsa/posters/1",
+        language: "Hindi",
+        releaseDate: "18-03-2022",
+        releaseSatus: "RELEASED"
+    });
+    await Movie.create({
+        name: "Jhund",
+        description: "Comedy Drama Movie",
+        casts: ["Amitabh Bachchan", "Abhinay Raj"],
+        director: "Nagraj Manjule",
+        trailerUrl: "http://jhund/trailers/1",
+        posterUrl: "http://jhund/posters/1",
+        language: "Hindi",
+        releaseDate: "04-03-2022",
+        releaseSatus: "RELEASED"
+    });
+    await Movie.create({
+        name: "Radhe Shyam",
+        description: "Comedy Drama Movie",
+        casts: ["Prabhas", "Pooja Hegde"],
+        director: "Radha Krishna Kumar",
+        trailerUrl: "http://RadheShyam/trailers/1",
+        posterUrl: "http://RadheShyam/posters/1",
+        language: "Hindi",
+        releaseDate: "11-03-2022",
+        releaseSatus: "RELEASED"
+    });
+    await Movie.create({
+        name: "The Kashmir Files",
+        description: "Intense Movie",
+        casts: ["Mithun Chakraborty", "Anupam Kher"],
+        director: "Vivek Agnihotri",
+        trailerUrl: "http://TheKashmirFiles/trailers/1",
+        posterUrl: "http://TheKashmirFiles/posters/1",
+        language: "Hindi",
+        releaseDate: "11-03-2022",
+        releaseSatus: "RELEASED"
+    });
+    console.log("Movies inserted in the db");
+}
+catch(err){
+    console.log('error while inserting default entries in DB', err);
+}
+}
 // call the routes
 require('./routes/movie.routes')(app);
 
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,5 +1,4 @@
 const { getAllMovies } = require('../controller/movies.controller');
-
 module.exports = (app) => {
-    app.get('/mba/api/v1/movies', getAllMovies)   
+    app.get('/mba/api/v1/movies', getAllMovies);   
 }
